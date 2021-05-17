84da8b78293f5b6d35dc0c6ee843df457b336343
j584lee98 <57649385+j584lee98@users.noreply.github.com>
2021-05-17 13:41:32 -0400

    Add stripplot with no jitter

--- seaborn/categorical/stripplot.py
+++ seaborn/categorical/stripplot.py
@@ -3,6 +3,8 @@ import seaborn as sns
 
 penguins = sns.load_dataset('penguins')
 
-sns.stripplot(data=penguins, x="species", y="bill_length_mm")
+sns.catplot(data=penguins, x="species", y="bill_length_mm")
+
+sns.catplot(data=penguins, x="species", y="bill_length_mm", jitter=False)
 
 plt.show()
